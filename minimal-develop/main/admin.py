5e2a181dd26ae8b7e9cde354b0c4f5bfd27ab132
Baybosunov chyngyz <baybosunov0411@gmail.com>
2022-03-22 11:43:12 +0700

    Revert "models"
    This reverts commit b282b1fd66b75ecf2895c82598abb2dec5a9ca01.

--- minimal-develop/main/admin.py
+++ minimal-develop/main/admin.py
@@ -7,10 +7,3 @@ class ConsultationAdmin(admin.ModelAdmin):
     list_display = ('client_name', 'phone_number')
     list_display_links = ('client_name', )
     ordering = ('-date_added', )
-
-@admin.register(models.CmmSlider)
-class CmmSliderAdmin(admin.ModelAdmin):
-    list_editable = ('is_published',)
-    image_display = ('image',)
-    list_display_links = ('id', 'title')
-    list_display_text =('text',)
